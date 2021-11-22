8c47402809e1844a2c69655aa65a97538940c1c7
Mulat <mulatmek@gmail.com>
2021-11-23 00:46:16 +0200

    Time series class finished

--- MULAT/src/test/TimeSeries.java
+++ MULAT/src/test/TimeSeries.java
@@ -3,9 +3,8 @@ import java.io.*;
 import java.util.Map;
 import java.util.HashMap;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Vector;
-import java.util.List;
+
 public class TimeSeries {
 	Map <String,Vector<Float> > my_map=new HashMap<>();
 	String line ="";
@@ -13,6 +12,7 @@ public class TimeSeries {
 	String temp[]=null;
 	Vector<Vector<Float>> calVal;
 	Vector<Float> temp_vec;
+	int numOf_vec;
 	
 	public TimeSeries(String csvFileName) {
 
@@ -24,40 +24,44 @@ public class TimeSeries {
 			while ((line =lineRead.readLine())!=null) {
 				if(rowCounter==0) {
 					header = line.split(",");
+					numOf_vec =header.length;
 				}
 
-
-
-
-				if(rowCounter>0){
+				else{
 					temp=line.split(",");
-					temp_vec=new Vector<>();
 					for (int i=0;i<temp.length;i++) {
+						if(calVal.size()!=temp.length) {
+							temp_vec = new Vector<>();
+							float f = Float.parseFloat(temp[i]);
+							temp_vec.add(f);
+							calVal.add(temp_vec);
+						}
+						else{
+							float f = Float.parseFloat(temp[i]);
+							calVal.get(i).add(f);
+						}
 
-						float f = Float.parseFloat(temp[i]);
-						temp_vec.add(f);
 					}
-					calVal.add(temp_vec);
+
 				}
 				rowCounter++;
-				if(rowCounter==4){
-				for (int i=0;i<3;i++) {
+				if(rowCounter== numOf_vec -1){
+				for (int i=0;i<temp.length;i++) {
 					my_map.put(header[i], calVal.get(i));
 				}
 				}
 			}
-
-
-
-
 
 		}catch (IOException e){
 		}
 
 	}
 
- public Vector<Float> return_col(String key){
+ public Vector<Float> get_column(String key){
 		return my_map.get(key);
  }
+	public float get_index(String key,int index){
+		return my_map.get(key).get(index-1);
+	}
 	
 }
